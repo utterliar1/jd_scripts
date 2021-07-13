f10cbae4e21008796eb25a485ae18a6d41247eaf
GitHub Action <action@github.com>
2021-07-13 15:31:52 +0000

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -140,15 +140,15 @@
 #点点券
 20 0,20 * * * node /scripts/jd_necklace.js >> /scripts/logs/jd_necklace.log 2>&1
 # 京东到家果园
-10 0,3,8,11,17 * * * node /scripts/jd_dj_fruit.js >> /scripts/logs/jd_dj_fruit.log 2>&1
+10 0,3,8,11,17 * * * node /scripts/jddj_fruit.js >> /scripts/logs/jd_dj_fruit.log 2>&1
 # 京东到家鲜豆任务
-10 0 * * * node /scripts/jd_dj_bean.js >> /scripts/logs/jd_dj_bean.log 2>&1
+10 0 * * * node /scripts/jddj_bean.js >> /scripts/logs/jd_dj_bean.log 2>&1
 # 京东到家果园水车收水滴
-*/5 * * * * node /scripts/jd_dj_fruit_collectWater.js >> /scripts/logs/jd_dj_fruit_collectWater.log 2>&1
+*/5 * * * * node /scripts/jddj_fruit_collectWater.js >> /scripts/logs/jd_dj_fruit_collectWater.log 2>&1
 # 京东到家鲜豆庄园
-10 0 * * * node /scripts/jd_dj_plantBeans.js >> /scripts/logs/jd_dj_plantBeans.log 2>&1
+10 0 * * * node /scripts/jddj_plantBeans.js >> /scripts/logs/jd_dj_plantBeans.log 2>&1
 # 京东到家鲜豆庄园收水滴
-*/5 * * * * node /scripts/jd_dj_getPoints.js >> /scripts/logs/jd_dj_getPoints.log 2>&1
+*/5 * * * * node /scripts/jddj_getPoints.js >> /scripts/logs/jd_dj_getPoints.log 2>&1
 # 多合一签到
 5 0 * * * node /scripts/JD_DailyBonus.js >> /scripts/logs/JD_DailyBonus.log 2>&1
 # 抽奖机
