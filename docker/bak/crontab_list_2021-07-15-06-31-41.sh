40ad488c89f69a56609cf10bee299cd06fe5c842
GitHub Action <action@github.com>
2021-07-15 06:31:54 +0000

    update

--- docker/bak/crontab_list_2021-07-15-06-31-41.sh
+++ docker/bak/crontab_list_2021-07-15-06-31-41.sh
@@ -135,8 +135,6 @@
 0 12 * * * node /scripts/jd_api_test.js >> /scripts/logs/jd_api_test.log 2>&1
 #点点券
 20 0,20 * * * node /scripts/jd_necklace.js >> /scripts/logs/jd_necklace.log 2>&1
-#京喜牧场
-20 0-23/3 * * *  node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
 # 京东到家果园（passby）
 10 0,3,8,11,17 * * * node /scripts/jd_dj_fruit.js >> /scripts/logs/jddj_fruit.log 2>&1
 # 京东到家鲜豆任务（passby）
