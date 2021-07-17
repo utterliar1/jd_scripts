a19e197955c77927a0f43fac07b4bd70edb83de8
utterliar1 <211538958@qq.com>
2021-07-17 19:07:15 +0800

    更新 diy.sh

--- docker/diy.sh
+++ docker/diy.sh
@@ -8,10 +8,6 @@
 10 0 * * * node /scripts/jd_dj_plantBeans.js >> /scripts/logs/jddj_plantBeans.log 2>&1
 # 京东到家鲜豆庄园收水滴（passby）
 */5 * * * * node /scripts/jd_dj_getPoints.js >> /scripts/logs/jddj_getPoints.log 2>&1
-# 多合一签到（NobyDa）
-5 0 * * * node /scripts/jd_DailyBonus.js >> /scripts/logs/jd_DailyBonus.log 2>&1
-# 抽奖机（yangtingxiao）
-11 1 * * * node /scripts/jd_lotteryMachine.js >> /scripts/logs/jd_lotteryMachine.log 2>&1
 # 滴滴果园（passby）
 10 0,8,12,18 * * * node /scripts/jd_dd_fruit.js >> /scripts/logs/jd_dd_fruit.log 2>&1
 # 快手果园（passby）
@@ -23,7 +19,11 @@
 0,1 16-23 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/jd_super_redrain.log 2>&1
 # 京东金融养猪猪（ak）
 12 0-23/6 * * * node /scripts/jd_task_pigPet.js >> /scripts/logs/jd_task_pigPet.log 2>&1
-# 财富大陆（smiek）
-#18 0-23/2 * * * node /scripts/jd_wealth_island.js >> /scripts/logs/jd_wealth_island.log 2>&1
 # 店铺签到（jiulan）
 #10 1,9 * * * node /scripts/jd_shop_sign.js >> /scripts/logs/jd_shop_sign.log 2>&1
+# 财富大陆（smiek）
+#18 0-23/2 * * * node /scripts/jd_wealth_island.js >> /scripts/logs/jd_wealth_island.log 2>&1
+# 燃动夏季（smiek）
+12 9,11,13,15,17 * * * node /scripts/jd_summer_movement.js >> /scripts/logs/jd_summer_movement.log 2>&1
+# 燃动夏季_SH助力（smiek）
+12 7-14 * * * node /scripts/jd_summer_movement_help.js >> /scripts/logs/jd_summer_movement_help.log 2>&1
