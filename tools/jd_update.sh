a6956755a56ad575d39b8d3a6408a05e0e49cea0
utterliar1 <211538958@qq.com>
2021-07-19 09:12:56 +0800

    强制更新

--- tools/jd_update.sh
+++ tools/jd_update.sh
@@ -1,12 +1,11 @@
-/**
- * 强制更新
- * cron=0 0-23/12 * * *
- */
-
-if [ ! -d "/ql" ];then
+#!/usr/bin/env bash
+# 强制更新
+if [ -s /jd ];then
   echo "========= 检查到V4环境，开始强制更新 ========="
   cd /jd/scripts/ && git fetch --all; git reset --hard origin/master; git pull && jup scripts
-else
+elif [ -s /ql ];then
   echo "========= 检查到青龙环境，开始强制更新 ========="
   cd /ql/repo/utterliar1_jd_scripts/ && git fetch --all; git reset --hard origin/master; git pull && ql repo https://github.com/utterliar1/jd_scripts.git "jd_" "utils|backup|tools|jxmcToken" "^jd[^_]|USER|package|jxmcToken"
+else
+  echo "========= 环境未知，请自行更新 ========="
 fi
