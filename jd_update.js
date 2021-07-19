42528ef944b559a2c1fa21542a15da813a8cfe3a
utterliar1 <211538958@qq.com>
2021-07-19 08:47:09 +0800

    更新 jd_update.js

--- jd_update.js
+++ jd_update.js
@@ -3,20 +3,10 @@
  * cron=0 0-23/12 * * *
  */
 
-const exec = require('child_process').exec;
-
-if(process.env.PWD==='/ql/scripts'){
-  exec("cd /ql/repo/JDHelloWorld_jd_scripts")
-}
-
-exec("git fetch --all; git reset --hard origin/main; git pull", (error, stdout, stderr) => {
-  console.log(stdout.trim())
-})
-
-if(process.env.PWD==='/ql/scripts') {
-  if (__dirname.indexOf('/ql/') > -1) {
-    exec('ql repo https://github.com/JDHelloWorld/jd_scripts.git "jd_|jx_|getCookie" "activity|backUp|Coupon|enen" "^jd[^_]|USER|tools"', (error, stdout, stderr) => {
-      console.log(stdout.trim())
-    })
-  }
-}
+if [ ! -d "/ql" ];then
+  echo "========= 检查到V4环境，开始强制更新 ========="
+  cd /jd/scripts/ && git fetch --all; git reset --hard origin/master; git pull && jup scripts
+else
+  echo "========= 检查到青龙环境，开始强制更新 ========="
+  cd /ql/repo/utterliar1_jd_scripts/ && git fetch --all; git reset --hard origin/master; git pull && ql repo https://github.com/utterliar1/jd_scripts.git "jd_" "utils|backup|tools|jxmcToken" "^jd[^_]|USER|package|jxmcToken"
+fi
