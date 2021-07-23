9ce6778d7a4d69624f3710a9f5b939c03846b6fd
GitHub Action <action@github.com>
2021-07-23 20:22:21 +0000

    update

--- backup/cron/crontab_list.sh
+++ backup/cron/crontab_list.sh
@@ -1,8 +1,3 @@
-# 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
-50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
-#收集助力码
-30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
-
 ##############短期活动##############
 #燃动夏季
 12 0,6-23/2 * * * node /scripts/jd_summer_movement.js  >> /scripts/logs/jd_summer_movement.log 2>&1
