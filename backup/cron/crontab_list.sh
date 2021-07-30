cda9e43ca2501245249d061b3627339cf8fefc83
GitHub Action <action@github.com>
2021-07-30 01:40:41 +0000

    update

--- backup/cron/crontab_list.sh
+++ backup/cron/crontab_list.sh
@@ -2,7 +2,6 @@
 #全民摸冰
 6 9,12 * * * node /scripts/jd_mb.js  >> /scripts/logs/jd_mb.log 2>&1
 #财富岛提现
-0 12,0 * * * node /scripts/jd_cfd_cashOut.js  >> /scripts/logs/jd_cfd_cashOut.log 2>&1
 #7月粉丝互动
 1 8 * * * node /scripts/jd_wxFans.js >> /scripts/logs/jd_wxFans.log 2>&1
 #星系牧场
