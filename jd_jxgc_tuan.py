7f39a9f1957b47a3260b4b774be5fbd425233371
GitHub Action <action@github.com>
2021-08-08 06:28:23 +0000

    update

--- jd_jxgc_tuan.py
+++ jd_jxgc_tuan.py
@@ -8,7 +8,7 @@ cron: 10 0,7 * * *
 Date: 2021/7/17 下午9:40
 TG交流 https://t.me/topstyle996
 TG频道 https://t.me/TopStyle2021
-update 2021.7.24 14:30
+update 2021.8.8 12:30
 建议cron: 0 0,7,10 * * *  python3 jd_jxgc_tuan.py
 new Env('京喜工厂开团');
 '''
@@ -37,7 +37,7 @@ from hashlib import sha256, sha512, md5
 import hmac
 
 appId = 10001
-activeId = 'T_zZaWP6by9yA1wehxM4mg%3D%3D'
+activeId = 'Xj2_3G-hQ4GRLCsLqIxFeQ%3D%3D'
 
 countElectric = {}
 def userAgent():
@@ -243,7 +243,7 @@ def getactiveId():
         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Mobile Safari/537.36"
     }
     result = requests.get(url, headers, timeout=30).text
-    r = re.compile(r'activeId=(T_.*?),')
+    r = re.compile(r'activeId=(Xj2_.*?),')
     r = r.findall(result)
     if len(r) > 0:
         activeId = r[0]
