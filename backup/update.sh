743a581b81da05aa640d2cf4658323425b6de737
utterliar1 <61038384+utterliar1@users.noreply.github.com>
2021-08-19 19:22:51 +0800

    Update update.sh

--- backup/update.sh
+++ backup/update.sh
@@ -5,7 +5,7 @@ if [ -s /jd ];then
   cd /jd/scripts/ && git fetch --all; git reset --hard origin/master; git pull && jup scripts
 elif [ -s /ql ];then
   echo "========= 检查到青龙环境，开始强制更新 ========="
-  cd /ql/repo/utterliar1_jd_scripts/ && git fetch --all; git reset --hard origin/master; git pull && ql repo https://github.com/utterliar1/jd_scripts.git "jd_" "utils|backup|tools" "^jd[^_]|USER|package|tools|utils"
+  cd /ql/repo/utterliar1_jd_scripts/ && git fetch --all; git reset --hard origin/master; git pull && ql repo https://github.com/utterliar1/jd_scripts.git "jd_" "utils|backup|tools|function" "^jd[^_]|USER|^TS|tools|function|utils"
 else
   echo "========= 环境未知，请自行更新 ========="
 fi
