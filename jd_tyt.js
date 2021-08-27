a700045e0a2a7e06b5a8273e7d396645b8016e43
GitHub Action <action@github.com>
2021-08-27 04:29:46 +0000

    update

--- jd_tyt.js
+++ jd_tyt.js
@@ -103,7 +103,7 @@ function tythelp() {
                     console.log("已完成砍价")
                     status = 1
                 } else {
-                    // console.log(data.msg)
+                    console.log(data.msg)
                 }
             } catch (e) {
                 $.logErr(e, resp);
