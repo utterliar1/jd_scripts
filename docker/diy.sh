b0a7266472e6ae40dd2e33655b5f4cb3d4cc2982
utterliar1 <61038384+utterliar1@users.noreply.github.com>
2021-09-06 11:01:36 +0800

    fix

--- docker/diy.sh
+++ docker/diy.sh
@@ -1,10 +1,12 @@
 # 半点京豆雨（龙珠）
-30,31 16-23 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/jd_half_redrain.log 2>&1
+#30,31 16-23 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/jd_half_redrain.log 2>&1
 &1
 # 整点京豆雨（龙珠）
 #0,1 16-23 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/jd_super_redrain.log 2>&1
+# 京东保价（ak）
+#48 */8 * * * node /scripts/jd_work_price.js >> /scripts/logs/jd_work_price.log 2>&1
 # 京东金融养猪猪（ak）
-12 0-23/6 * * * node /scripts/jd_task_pigPet.js >> /scripts/logs/jd_task_pigPet.log 2>&1
+12 0-23/6 * * * node /scripts/jd_work_pigPet.js >> /scripts/logs/jd_work_pigPet.log 2>&1
 # 京喜财富岛提现（小小）
 #59 11,12,23 * * * node /scripts/jd_cfdtx.js >> /scripts/logs/jd_cfdtx.log 2>&1
 # 伊利养牛（HW）
