841c71fc47038ca31a5ec72d2577316a5474c7b7
GitHub Action <action@github.com>
2021-10-21 14:19:20 +0000

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,6 +5,9 @@
 
 ##############短期活动##############
 
+# 环游记
+13 0,6-23/2 * 10-11 * node /scripts/jd_hyj.js >> /scripts/logs/jd_hyj.log 2>&1
+
 # 惊喜大作战
 2 0,2 * 10-11 * node /scripts/jd_ys.js >> /scripts/logs/jd_ys.log 2>&1
 
