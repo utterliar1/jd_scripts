f864200915256c1aab87d37825175032386da8a2
GitHub Action <action@github.com>
2021-10-23 06:23:36 +0000

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -32,9 +32,6 @@
 # 京东小魔方 (9.29 24.00 结束)
 13 0,10 * * * node /scripts/jd_mf.js >> /scripts/logs/jd_mf.log 2>&1
 
-# 蚊子腿豆子 (9.21-10.16 10月16号应该可以参与瓜分)
-3 6,18 1-16,21-30 9,10 * node /scripts/jd_decompression.js >> /scripts/logs/jd_decompression.log 2>&1
-
 # 芥么赚豪礼
 22 1,13 * * * node /scripts/jd_genz.js >> /scripts/logs/jd_genz.log 2>&1
 
