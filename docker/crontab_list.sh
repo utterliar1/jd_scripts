76dd4aca4f8116ca32daf8651ec253e240fe4029
GitHub Action <action@github.com>
2021-11-27 10:22:23 +0000

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -81,6 +81,8 @@
 5 0,9 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
 # 领券中心签到
 17 0 * * * node /scripts/jd_ccSign.js >> /scripts/logs/jd_ccSign.log 2>&1
+# 翻牌签到
+3 1,20 * * * node /scripts/jd_fanpai_sign.js >> /scripts/logs/jd_fanpai_sign.log 2>&1
 # 过期京豆兑换喜豆
 22 9 * * * node /scripts/jd_exchangejxbeans.js >> /scripts/logs/jd_exchangejxbeans.log 2>&1
 # 东东世界
