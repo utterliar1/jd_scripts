5ab2938560633555e3fcc9872747d27ed11a747e
GitHub Action <action@github.com>
2021-12-10 16:32:42 +0000

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -11,6 +11,9 @@
 # 魔方兑换
 11 0,20 * * * node /scripts/jd_mf_exchange.js >> /scripts/logs/jd_mf_exchange.log 2>&1
 
+# 发现好货 (12.31)
+33 10,16 * 12 * node /scripts/jd_fxhh.js >> /scripts/logs/jd_fxhh.log 2>&1
+
 # 跳跳乐瓜分京豆 (什么时候结束)
 # 1 0,9,15,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 
