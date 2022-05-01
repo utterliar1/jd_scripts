7cd20ef2f41dba93a6cbe8a4ef3c3bee3698d93d
galabug <153454219@qq.com>
2022-05-02 02:43:04 +0800

    更新

--- jd_speed_redpocket.js
+++ jd_speed_redpocket.js
@@ -1,176 +1,57 @@
-"use strict";
 /**
  * 极速版-领红包
  */
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __values = (this && this.__values) || function(o) {
-    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
-    if (m) return m.call(o);
-    if (o && typeof o.length === "number") return {
-        next: function () {
-            if (o && i >= o.length) o = void 0;
-            return { value: o && o[i++], done: !o };
-        }
-    };
-    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
-};
-var __read = (this && this.__read) || function (o, n) {
-    var m = typeof Symbol === "function" && o[Symbol.iterator];
-    if (!m) return o;
-    var i = m.call(o), r, ar = [], e;
+
+// import {requireConfig, wait, get, o2s} from './TS_USER_AGENTS'
+// import {H5ST} from "./utils/h5st"
+var {requireConfig, wait, get, o2s} = require("./TS_USER_AGENTS2");
+var {H5ST} = require("./utils/h5st");
+
+let cookie = '', res = '', UserName = '', h5stTool = new H5ST("07244", "jdltapp;", "5817062902662730")
+
+!(async () => {
+  let cookiesArr = await requireConfig()
+  for (let [index, value] of cookiesArr.entries()) {
+    cookie = value
+    UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)[1])
+    console.log(`\n开始【京东账号${index + 1}】${UserName}\n`)
     try {
-        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
-    }
-    catch (error) { e = { error: error }; }
-    finally {
+      await h5stTool.__genAlgo()
+      res = await api('spring_reward_query', {"linkId": "Eu7-E0CUzqYyhZJo9d3YkQ", "inviter": ""})
+      let remainChance = res.data.remainChance
+      console.log('剩余抽奖次数：', remainChance)
+      for (let i = 0; i < remainChance; i++) {
+        res = await api('spring_reward_receive', {"inviter": "", "linkId": "Eu7-E0CUzqYyhZJo9d3YkQ"})
         try {
-            if (r && !r.done && (m = i["return"])) m.call(i);
+          console.log('抽奖成功', res.data.received.prizeDesc)
+        } catch (e) {
+          console.log('抽奖失败')
+          break
         }
-        finally { if (e) throw e.error; }
+        await wait(2000)
+      }
+    } catch (e) {
+      console.log('火爆')
     }
-    return ar;
-};
-exports.__esModule = true;
-var TS_USER_AGENTS_1 = require("./TS_USER_AGENTS");
-var h5st_1 = require("./utils/h5st");
-var cookie = '', res = '', UserName = '', h5stTool = new h5st_1.H5ST("07244", "jdltapp;", "5817062902662730");
-!(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var cookiesArr, _a, _b, _c, index, value, remainChance, i, e_1, e_2_1;
-    var e_2, _d;
-    return __generator(this, function (_e) {
-        switch (_e.label) {
-            case 0: return [4 /*yield*/, (0, TS_USER_AGENTS_1.requireConfig)()];
-            case 1:
-                cookiesArr = _e.sent();
-                _e.label = 2;
-            case 2:
-                _e.trys.push([2, 17, 18, 19]);
-                _a = __values(cookiesArr.entries()), _b = _a.next();
-                _e.label = 3;
-            case 3:
-                if (!!_b.done) return [3 /*break*/, 16];
-                _c = __read(_b.value, 2), index = _c[0], value = _c[1];
-                cookie = value;
-                UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)[1]);
-                console.log("\n\u5F00\u59CB\u3010\u4EAC\u4E1C\u8D26\u53F7".concat(index + 1, "\u3011").concat(UserName, "\n"));
-                _e.label = 4;
-            case 4:
-                _e.trys.push([4, 12, , 13]);
-                return [4 /*yield*/, h5stTool.__genAlgo()];
-            case 5:
-                _e.sent();
-                return [4 /*yield*/, api('spring_reward_query', { "linkId": "Eu7-E0CUzqYyhZJo9d3YkQ", "inviter": "" })];
-            case 6:
-                res = _e.sent();
-                remainChance = res.data.remainChance;
-                console.log('剩余抽奖次数：', remainChance);
-                i = 0;
-                _e.label = 7;
-            case 7:
-                if (!(i < remainChance)) return [3 /*break*/, 11];
-                return [4 /*yield*/, api('spring_reward_receive', { "inviter": "", "linkId": "Eu7-E0CUzqYyhZJo9d3YkQ" })];
-            case 8:
-                res = _e.sent();
-                try {
-                    console.log('抽奖成功', res.data.received.prizeDesc);
-                }
-                catch (e) {
-                    console.log('抽奖失败');
-                    return [3 /*break*/, 11];
-                }
-                return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(2000)];
-            case 9:
-                _e.sent();
-                _e.label = 10;
-            case 10:
-                i++;
-                return [3 /*break*/, 7];
-            case 11: return [3 /*break*/, 13];
-            case 12:
-                e_1 = _e.sent();
-                console.log('火爆');
-                return [3 /*break*/, 13];
-            case 13: return [4 /*yield*/, (0, TS_USER_AGENTS_1.wait)(5000)];
-            case 14:
-                _e.sent();
-                _e.label = 15;
-            case 15:
-                _b = _a.next();
-                return [3 /*break*/, 3];
-            case 16: return [3 /*break*/, 19];
-            case 17:
-                e_2_1 = _e.sent();
-                e_2 = { error: e_2_1 };
-                return [3 /*break*/, 19];
-            case 18:
-                try {
-                    if (_b && !_b.done && (_d = _a["return"])) _d.call(_a);
-                }
-                finally { if (e_2) throw e_2.error; }
-                return [7 /*endfinally*/];
-            case 19: return [2 /*return*/];
-        }
-    });
-}); })();
-function api(fn, body) {
-    return __awaiter(this, void 0, void 0, function () {
-        var timestamp, h5st;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    timestamp = Date.now();
-                    h5st = h5stTool.__genH5st({
-                        appid: 'activities_platform',
-                        body: JSON.stringify(body),
-                        client: 'H5',
-                        clientVersion: '1.0.0',
-                        functionId: fn,
-                        t: timestamp.toString()
-                    });
-                    return [4 /*yield*/, (0, TS_USER_AGENTS_1.get)("https://api.m.jd.com/?functionId=".concat(fn, "&body=").concat(encodeURIComponent(JSON.stringify(body)), "&t=").concat(timestamp, "&appid=activities_platform&h5st=").concat(h5st), {
-                            'Host': 'api.m.jd.com',
-                            'Origin': 'https://prodev.m.jd.com',
-                            'User-Agent': 'jdltapp;',
-                            'Referer': 'https://prodev.m.jd.com/',
-                            'Cookie': cookie
-                        })];
-                case 1: return [2 /*return*/, _a.sent()];
-            }
-        });
-    });
+    await wait(5000)
+  }
+})()
+
+async function api(fn, body) {
+  let timestamp = Date.now()
+  let h5st = h5stTool.__genH5st({
+    appid: 'activities_platform',
+    body: JSON.stringify(body),
+    client: 'H5',
+    clientVersion: '1.0.0',
+    functionId: fn,
+    t: timestamp.toString(),
+  })
+  return await get(`https://api.m.jd.com/?functionId=${fn}&body=${encodeURIComponent(JSON.stringify(body))}&t=${timestamp}&appid=activities_platform&h5st=${h5st}`, {
+    'Host': 'api.m.jd.com',
+    'Origin': 'https://prodev.m.jd.com',
+    'User-Agent': 'jdltapp;',
+    'Referer': 'https://prodev.m.jd.com/',
+    'Cookie': cookie
+  })
 }
